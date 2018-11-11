f62e3504233dd3db7ba6883e9294a3880bd3e719
Valentin Bru <bruvalentin84@icloud.com>
2018-11-11 11:15:10 +0100

    adding the button of all the items

--- pages/play.php
+++ pages/play.php
@@ -45,23 +45,53 @@ and open the template in the editor.
             //Nombre de Mines
             var NBhamster = <?php echo $_SESSION['hamster']; ?>;
             var NBraspy = <?php echo $_SESSION['raspi']; ?>;
-
+            var NBpcport = <?php echo $_SESSION['pcportable']; ?>;
+            var NBrig = <?php echo $_SESSION['rigperso']; ?>;
+            var NBsalle = <?php echo $_SESSION['salle']; ?>;
+            var NBbuanderie = <?php echo $_SESSION['buanderie']; ?>;
+            var NBserveur = <?php echo $_SESSION['serveur']; ?>;
+            
+            
             var prixhasmter = 20;
             var prixraspy = 100;
+            var prixpcport = 400;
+            var prixrig = 800;
+            var prixsalle = 1000;
+            var prixbuand = 10000;
+            var prixserv = 50000;
+           
 
             var Vraspy = 5;
+            var Vpcport = 10;
+            var Vrig = 15;
+            var Vsalle = 20;
+            var Vbuand = 25;
+            var Vserv = 30;
+           
 
 
             var timehamster = 3;
             var timeraspy = 10;
+            var timepcport = 20;
+            var timerig = 30;
+            var timesalle = 40;
+            var timebuand = 50;
+            var timeserv = 60;
+            
             var timedEvent;
             function preload()
             {
                 this.load.image('bitcoin', '../assets/bitcoin.png');
                 this.load.image('hamster', '../assets/hamster.png');
                 this.load.image('raspy', '../assets/raspy.png');
+                this.load.image('pcport', '../assets/pcport.png');
+                this.load.image('rig', '../assets/RIGperso.png');
+                this.load.image('salle', '../assets/salle.png');
+                this.load.image('buand', '../assets/buanderie.png');
+                this.load.image('serv', '../assets/serveur.png');
+                
                 this.load.image('save', '../assets/save.png');
-                this.load.image('part', '../assets/bit.png');
+               
             }
 
             function create()
@@ -71,8 +101,14 @@ and open the template in the editor.
                 var sprite = this.add.sprite(450, 400, 'bitcoin').setInteractive();
                 var Bhamster = this.add.sprite(975, 56, 'hamster').setInteractive();
                 var Braspy = this.add.sprite(975, 150, 'raspy').setInteractive();
-                var Bsave = this.add.sprite(975, 400, 'save').setInteractive();
-                var particles = this.add.particles('part');
+                var Bpcport = this.add.sprite(975, 242, 'pcport').setInteractive();
+                var Brig = this.add.sprite(975, 334, 'rig').setInteractive();
+                var Bsalle = this.add.sprite(975, 427, 'salle').setInteractive();
+                var Bbuand = this.add.sprite(975, 520, 'buand').setInteractive();
+                var Bserv = this.add.sprite(975, 613, 'serv').setInteractive();
+                
+                var Bsave = this.add.sprite(100, 600, 'save').setInteractive();
+                
 
 
 
@@ -189,9 +225,137 @@ and open the template in the editor.
                 });
                 var Rtext = this.add.text(770, 129, '0', {fontSize: '30px', fill: 'white'});
                 this.add.text(975, 149, '100B Raspy', {fontSize: '20px', fill: 'white'});
+                
+                //Boutton pcportable
+                Bpcport.on('pointerdown', function (pointer) {
 
+                    if (bit >= prixpcport) {
+                        NBpcport = NBpcport + 1;
+                        bit = bit - prixpcport;
+                        PCtext.setText('' + NBpcport);
+                        this.setTint(0x7878ff);
+                    } else
+                    {
+                        this.setTint(0xff0000);
+                    }
+                });
+                Bpcport.on('pointerout', function (pointer) {
 
+                    this.clearTint();
 
+                });
+                Bpcport.on('pointerup', function (pointer) {
+
+                    this.clearTint();
+
+                });
+                var PCtext = this.add.text(770, 220, '0', {fontSize: '30px', fill: 'white'});
+                this.add.text(975, 242, '400B Pcportable', {fontSize: '20px', fill: 'white'});
+                
+                //Boutton rig
+                Brig.on('pointerdown', function (pointer) {
+
+                    if (bit >= prixrig) {
+                        NBrig = NBrig + 1;
+                        bit = bit - prixrig;
+                        RGtext.setText('' + NBrig);
+                        this.setTint(0x7878ff);
+                    } else
+                    {
+                        this.setTint(0xff0000);
+                    }
+                });
+                Brig.on('pointerout', function (pointer) {
+
+                    this.clearTint();
+
+                });
+                Brig.on('pointerup', function (pointer) {
+
+                    this.clearTint();
+
+                });
+                var RGtext = this.add.text(770, 310, '0', {fontSize: '30px', fill: 'white'});
+                this.add.text(975, 334, '400B Rig Perso', {fontSize: '20px', fill: 'white'});
+                
+                //Boutton salle
+                Bsalle.on('pointerdown', function (pointer) {
+
+                    if (bit >= prixsalle) {
+                        NBsalle = NBsalle + 1;
+                        bit = bit - prixsalle;
+                        Stext.setText('' + NBsalle);
+                        this.setTint(0x7878ff);
+                    } else
+                    {
+                        this.setTint(0xff0000);
+                    }
+                });
+                Bsalle.on('pointerout', function (pointer) {
+
+                    this.clearTint();
+
+                });
+                Bsalle.on('pointerup', function (pointer) {
+
+                    this.clearTint();
+
+                });
+                var Stext = this.add.text(770, 400, '0', {fontSize: '30px', fill: 'white'});
+                this.add.text(975, 427, '800B Salle Info', {fontSize: '20px', fill: 'white'});
+                
+                 //Boutton buand
+                Bbuand.on('pointerdown', function (pointer) {
+
+                    if (bit >= prixbuand) {
+                        NBbuand = NBbuand + 1;
+                        bit = bit - prixbuand;
+                        Btext.setText('' + NBbuand);
+                        this.setTint(0x7878ff);
+                    } else
+                    {
+                        this.setTint(0xff0000);
+                    }
+                });
+                Bbuand.on('pointerout', function (pointer) {
+
+                    this.clearTint();
+
+                });
+                Bbuand.on('pointerup', function (pointer) {
+
+                    this.clearTint();
+
+                });
+                var Btext = this.add.text(770, 500, '0', {fontSize: '30px', fill: 'white'});
+                this.add.text(975, 520, '10000B Buanderie', {fontSize: '20px', fill: 'white'});
+                
+                //Boutton serveur
+                Bserv.on('pointerdown', function (pointer) {
+
+                    if (bit >= prixserv) {
+                        NBserv = NBserv + 1;
+                        bit = bit - prixserv;
+                        SEtext.setText('' + NBserv);
+                        this.setTint(0x7878ff);
+                    } else
+                    {
+                        this.setTint(0xff0000);
+                    }
+                });
+                Bserv.on('pointerout', function (pointer) {
+
+                    this.clearTint();
+
+                });
+                Bserv.on('pointerup', function (pointer) {
+
+                    this.clearTint();
+
+                });
+                var SEtext = this.add.text(770, 590, '0', {fontSize: '30px', fill: 'white'});
+                this.add.text(975, 613, '50000B Serveur', {fontSize: '20px', fill: 'white'});
+                
                 timedEvent = this.time.addEvent({delay: 500, callback: onEvent, callbackScope: this, loop: true});
             }
             function update()
